4e24f8f759d41cb9de2d4edb6a0501bd67c493d4
root <root@localhost.localdomain>
2021-03-10 10:45:46 +0700

    code php automation test

---
+++ Staff.php
@@ -0,0 +1,13 @@
+<?php
+
+class Staff
+{
+    public $first_name;
+    public $last_name;
+
+    public function getFullName()
+    {
+        return trim("$this->first_name $this->last_name");
+    }
+}
+?>
